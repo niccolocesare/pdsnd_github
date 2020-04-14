f30b3f1f038aaef19eb83a7ceba946e89b1bb9d3
Niccolò-Cesare <cesarenicco95@gmail.com>
2020-04-14 11:02:38 +0200

    Improving line 30 formatting

--- bikeshare.py
+++ bikeshare.py
@@ -27,7 +27,7 @@ def get_filters():
         try:
             city = input('\nWould you like to see data for Chicago, New York City, or Washington? \n>> ').lower()
             if city in CITY_DATA:
-                print(f'{city.title()} is the city you have selected!')
+                print('{} is the city you have selected!'.format(city.title()))
                 break
             elif city not in CITY_DATA:
                 print('\nWrong city value. Please retry.')
